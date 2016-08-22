df2b387e638a4a613a4891fd06376c86090cb4c3
Olaolu Olawuyi <whiskydee@gmail.com>
2016-08-22 17:31:51 +0200

    Add script.js

---
+++ js/src/script.js
@@ -0,0 +1,10 @@
+// document.addEventListener('DOMContentLoaded', function() {
+//   var btns = document.querySelectorAll('.edit button');
+//
+//   btns.forEach(function(btn) {
+//     btn.addEventListener('click', function() {
+//       btn.classList.toggle('clicked');
+//     });
+//   });
+//
+// });
