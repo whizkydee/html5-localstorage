11002b97e17ec5d47a8594edd34e6a820355ee31
Olaolu Olawuyi <whiskydee@gmail.com>
2016-09-30 20:19:22 +0100

    Log to console on sass error in sass gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ var sourcemaps    =     require('gulp-sourcemaps');
 gulp.task('sass', function() {
   return gulp.src('sass/**/*.scss')
     .pipe(sourcemaps.init())
-    .pipe(sass())
+    .pipe(sass().on('error', sass.logError))
     .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest(''))
 });
