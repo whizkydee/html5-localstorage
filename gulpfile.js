9685892106555559ba1be1be2a44f80647829699
Olaolu Olawuyi <whiskydee@gmail.com>
2016-10-05 20:24:28 +0200

    Re-order folder structure

--- gulpfile.js
+++ gulpfile.js
@@ -3,26 +3,30 @@ var babel         =     require('gulp-babel');
 var sass          =     require('gulp-sass');
 var sourcemaps    =     require('gulp-sourcemaps');
 
+// Assets source files path
+var sassSrc       =     'src/sass/**/*.scss';
+var jsSrc         =     'src/js/**/*.js';
+
 gulp.task('sass', function() {
-  return gulp.src('sass/**/*.scss')
+  return gulp.src(sassSrc)
     .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
     .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest(''))
+    .pipe(gulp.dest('dist/css'))
 });
 
 gulp.task('transpile', function() {
-  return gulp.src('js/src/**/*.js')
+  return gulp.src(jsSrc)
   .pipe(sourcemaps.init())
   .pipe(babel())
   .pipe(sourcemaps.write('.'))
-  .pipe(gulp.dest('js/dist'));
+  .pipe(gulp.dest('dist/js'));
 });
 
 
 gulp.task('watch', function() {
-  gulp.watch('js/src/**/*.js', ['transpile']);
-  gulp.watch('sass/**/*.scss', ['sass']);
+  gulp.watch(jsSrc, ['transpile']);
+  gulp.watch(sassSrc, ['sass']);
 });
 
 gulp.task('default', ['watch']);
